d2795692a06919453ebb0bad4367d7971d7777c2
Will B <wberman27@gmail.com>
2021-04-09 13:12:31 -0400

    added index and server foundations

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(helmet())
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+server.get('/', (req, res)=>{
+    res.send(`<h1>Sprint Challenge API</h1>`)
+});
+
+
 
 module.exports = server;
